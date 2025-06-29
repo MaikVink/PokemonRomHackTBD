e77f6f4354f46ed877fc8fb46addc2a82a894949
dwolf <dragonwolf2b@gmail.com>
2025-06-29 22:20:10 +0200

    WIP (Deneith change

--- src/data/object_events/object_event_pic_tables.h
+++ src/data/object_events/object_event_pic_tables.h
@@ -1260,3 +1260,7 @@ static const struct SpriteFrameImage sPicTable_RubySapphireBrendan[] = {
 static const struct SpriteFrameImage sPicTable_RubySapphireMay[] = {
     overworld_ascending_frames(gObjectEventPic_RubySapphireMayNormal, 2, 4),
 };
+
+static const struct SpriteFrameImage sPicTable_Deneith[] = {
+    overworld_ascending_frames(gObjectEventPic_Deneith, 2, 4),
+};
